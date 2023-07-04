515f7ba2dc5090eb73c78102f7fa1c7c5e62ecbc
NoName <afiffaiyaz398@gmail.com>
2023-07-04 13:39:30 -0400

    Downloaded node.js also learned about problem solving skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+if (x == 23) console.log(23);
+
+const calcAge = (birthYear) => 2023 - birthYear;
+
+console.log(calcAge(2004));
+
+console.log("My name is Afif");
