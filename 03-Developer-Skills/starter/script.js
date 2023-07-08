6f9db5aa4088602d43ee4eafe0e8bf60e27bd375
NoName <afiffaiyaz398@gmail.com>
2023-07-08 10:40:20 -0400

    Learned about how to properly solve a problem and how to use internet to solve problems

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -10,3 +10,51 @@ const calcAge = (birthYear) => 2023 - birthYear;
 console.log(calcAge(2004));
 
 console.log("My name is Afif");
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmp = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  return max - min;
+};
+
+const amp = calcTempAmp(temperatures);
+console.log(amp);
+//Problem 2 two arrays need to be passed on
+const calcTempAmpNew = function (temp1, temp2) {
+  //array1.concat(array2) - Mergees two arrays or more
+  const temps = temp1.concat(temp2);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  return max - min;
+};
+
+const ampNew = calcTempAmpNew(temperatures, [3, 5, 1, 4, 3, -5, 1 - 3, 14, 18]);
+console.log(ampNew);
+
+const measureKelvin = function () {
+  const measure = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degrees celsius: ")),
+  };
+
+  console.table(measure);
+  //   console.log(measure);
+  //   console.log(measure.value);
+
+  const kelvin = measure.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
