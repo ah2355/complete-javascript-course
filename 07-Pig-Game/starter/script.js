2bb9b9218c119b40e0c102dba5c3a9ffc1097e86
Afif Haque <afifhaque@Afifs-MacBook-Air.local>
2024-08-07 13:44:55 -0400

    Finished Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -11,11 +11,9 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+
+ 
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -25,11 +23,29 @@ const switchPlayer = function () {
     player1El.classList.toggle('player--active');
 }
 
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+ 
+let scores, currentScore, activePlayer, playing;
 
+const init = function(){
+     scores = [0,0];
+     currentScore = 0;
+     activePlayer = 0;
+     playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+
+}
+
+init();
 //Rolling Dice functionality
 btnRoll.addEventListener('click', function (){
     if(playing){
@@ -82,3 +98,8 @@ btnHold.addEventListener('click', function(){
     }
     }
 })
+
+
+btnNew.addEventListener('click', function(){
+    init();
+});
